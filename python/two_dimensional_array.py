811696c9646e506fba05ba2fa6eac38d931040ca
Jake Tiongson <jkarw7n@outlook.ph>
2026-01-28 16:35:01 +0800

    Create two_dimensional_array.py

---
+++ python/two_dimensional_array.py
@@ -0,0 +1,8 @@
+array = [
+    ["Kawesqar", "Chile"],
+    ["Nivkh", "Russia"],
+    ["Livonian", "Latvia"]
+]
+
+for language, country in array:
+    print(f"{language} is a language spoken in {country}.")

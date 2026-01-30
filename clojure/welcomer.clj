822af311af0644e76490834ffe7c939b9ee9eb6d
Jake Tiongson <jkarw7n@outlook.ph>
2026-01-30 15:16:18 +0800

    Create welcomer.clj

---
+++ clojure/welcomer.clj
@@ -0,0 +1,10 @@
+(ns clojure.welcomer)
+
+(defn welcome [& people]
+  (doseq [person people]
+    (println (clojure.string/join "" ["Hello " person "!"]))
+  )
+)
+
+(welcome "Mark" "Allyszah" "Paul Jan" "Ricardo")
+; welcomes each person in the arguments

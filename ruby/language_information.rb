f87f54fb272cbe7811249b4edcbc32143c3fd876
Jake Tiongson <jkarw7n@outlook.ph>
2026-02-03 01:38:50 +0800

    Create language_information.rb

---
+++ ruby/language_information.rb
@@ -0,0 +1,17 @@
+class LanguageInformation
+  attr_accessor :name, :family, :country, :speakers
+  def initialize(name, family, country, speakers=-1)
+    @name = name
+    @family = family
+    @country = country
+    @speakers = speakers
+  end
+  def print_info
+    puts "#{@name} is #{['a','e','i','o','u'].include?(@family.downcase[0]) ? "an" : "a"} #{@family} language spoken #{@speakers >= 0 ? "by #{@speakers} people " : ""}in #{@country}."
+  end
+end
+
+kawesqar = Language.new "Kawésqar", "Alacalufan", "Chile", 10
+kawesqar.print_info
+kawesqar.speakers=-1
+kawesqar.print_info
